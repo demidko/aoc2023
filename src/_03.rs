0012228eadbf1315cb0fd1c06bbd9c674ec0199e
Daniil Demidko <demidko@farpost.com>
2023-12-04 03:14:28 +0600

    draft...

--- src/_03.rs
+++ src/_03.rs
@@ -22,7 +22,111 @@ fn part_one() {
 
 #[test]
 fn part_two() {
+    let mut answer =
+        include_str!("../03.input")
+            .trim()
+            .lines()
+            .collect_vec();
+    let len = answer[0].len();
+    let fake_line = ".".repeat(len);
+    answer.insert(0, &fake_line);
+    answer.push(&fake_line);
+    let answer =
+        answer.windows(3)
+            .map(count_gears_ratio)
+            .sum::<i128>();
+    println!("Part Two answer is {}", answer)
+}
 
+fn count_gears_ratio(triple_windows: &[&str]) -> i128 {
+    let top = triple_windows[0];
+    let current = triple_windows[1];
+    let bottom = triple_windows[2];
+    let gears_indices = find_gears_indices(current);
+    let mut ratio_sum = 0;
+    for i in gears_indices {
+        let top_number = find_number_for_gear(top, i);
+        let bottom_number = find_number_for_gear(bottom, i);
+        if let (Some(top_number), Some(bottom_number)) = (top_number, bottom_number) {
+            let ratio = top_number * bottom_number;
+            ratio_sum += ratio;
+        }
+    }
+    ratio_sum
+}
+
+fn find_number_for_gear(s: &str, gear_idx: usize) -> Option<i128> {
+    let number_indices = find_possible_number_indices(gear_idx, s.len() - 1);
+    for i in number_indices {
+        let parsed_number = try_parse_number(s, i);
+        if parsed_number.is_some() {
+            return parsed_number;
+        }
+    }
+    None
+}
+
+fn try_parse_number(s: &str, any_idx: usize) -> Option<i128> {
+    let char = char_at(s, any_idx);
+    if char.is_ascii_digit() {
+        let number =
+            format!("{}{}{}",
+                    digits_before(s, any_idx),
+                    char,
+                    digits_after(s, any_idx)
+            );
+        return number.parse::<i128>().ok();
+    }
+    None
+}
+
+fn digits_before(s: &str, any_idx: usize) -> String {
+    if any_idx == 0 {
+        return String::from("");
+    }
+    let s = s[0..any_idx].chars().rev().collect::<String>();
+    let mut stop = s.len();
+    for (i, c) in s.char_indices() {
+        if c.is_ascii_digit().not() {
+            stop = i;
+            break;
+        }
+    }
+    String::from(&s[0..stop])
+}
+
+fn digits_after(s: &str, any_idx: usize) -> &str {
+    if any_idx == s.len() - 1 {
+        return "";
+    }
+    let s = &s[any_idx + 1..];
+    let mut stop = s.len();
+    for (i, c) in s.char_indices() {
+        if c.is_ascii_digit().not() {
+            stop = i;
+            break;
+        }
+    }
+    &s[0..stop]
+}
+
+fn find_possible_number_indices(gear_idx: usize, limit: usize) -> Vec<usize> {
+    let mut result = Vec::new();
+    if gear_idx > 0 {
+        result.push(gear_idx - 1)
+    }
+    result.push(gear_idx);
+    if gear_idx < limit {
+        result.push(gear_idx + 1)
+    }
+    result
+}
+
+fn find_gears_indices(s: &str) -> Vec<usize> {
+    s.char_indices()
+        .filter(|(i, c)| *c == '*')
+        .map(|(i, c)| i)
+        .collect_vec()
 }
 
 fn count_details(triple_windows: &[&str]) -> i128 {
